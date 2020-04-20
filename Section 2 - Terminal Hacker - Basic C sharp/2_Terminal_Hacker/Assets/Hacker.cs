3fe0bd07f2ed12065f45dd1bbdda38e8f263a901
Edvonilson "Zell" Pereira (mac-mini) <z-caps@hotmail.com>
2020-04-20 01:18:21 -0300

    ...

--- Section 2 - Terminal Hacker - Basic C sharp/2_Terminal_Hacker/Assets/Hacker.cs
+++ Section 2 - Terminal Hacker - Basic C sharp/2_Terminal_Hacker/Assets/Hacker.cs
@@ -1,49 +1,54 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
     // Start is called before the first frame update
-    
-        
+
+    //Game State
+    int level;
+
+    enum Screen = {}
+
     void Start()
     {
-        
+
         ShowMainMenu();
     }
-       
-                     
-    void ShowMainMenu ()
+
+
+    void ShowMainMenu()
     {
         Terminal.ClearScreen();
         Terminal.WriteLine("Qual o seu nível Hacker?");
         Terminal.WriteLine("Aperte 1 para easy!");
         Terminal.WriteLine("Aperte 2 para normal!");
-        Terminal.WriteLine("Aperte 3 para Hard!");
-        Terminal.WriteLine("");
         Terminal.WriteLine("Digite a sua opção");
     }
 
     void OnUserInput(string input)
     {
-       if (input == "menu")
+        if (input == "menu")
         {
             ShowMainMenu();
         }
 
         else if (input == "1")
         {
-            Terminal.WriteLine("você escolheu o level 1");
+            level = 1;
+            StartGame();
         }
 
         else if (input == "2")
         {
-            Terminal.WriteLine("você escolheu o level 2");
+            level = 2;
+            StartGame();
         }
-        else if (input == "3")
+        else if (input == "007")
         {
-            Terminal.WriteLine("você escolheu o level 3");
+            Terminal.WriteLine("Por favor escolha um level Mr. Bond!");
         }
         else
         {
@@ -52,9 +57,10 @@ public class Hacker : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void StartGame()
     {
-        
+        Terminal.WriteLine("você escolheu o level " + level);
+        Terminal.WriteLine("Digite o password: ");
     }
+
 }
